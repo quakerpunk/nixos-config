3ffc854a5eab2fcdb316cd4f42d89944fcdd4b52
Shawn Borton <shawn@shilohsvillage.com>
2024-02-05 18:29:57 -0600

    Hyprland - Workspaces

--- wm/hyprland/hyprland.nix
+++ wm/hyprland/hyprland.nix
@@ -82,6 +82,36 @@
       bind=ALTSHIFT,TAB,cyclenext,prev
       bind=ALTSHIFT,TAB,bringactivetotop
 
+      bind=$mainMod,H,movefocus,l
+      bind=$mainMod,J,movefocus,d
+      bind=$mainMod,K,movefocus,u
+      bind=$mainMod,L,movefocus,r
+
+      bind=$mainMod SHIFT,H,movewindow,l
+      bind=$mainMod SHIFT,J,movewindow,d
+      bind=$mainMod SHIFT,K,movewindow,u
+      bind=$mainMod SHIFT,L,movewindow,r
+
+      bind=$mainMod,1,exec,hyprworkspace 1
+      bind=$mainMod,2,exec,hyprworkspace 2
+      bind=$mainMod,3,exec,hyprworkspace 3
+      bind=$mainMod,4,exec,hyprworkspace 4
+      bind=$mainMod,5,exec,hyprworkspace 5
+      bind=$mainMod,6,exec,hyprworkspace 6
+      bind=$mainMod,7,exec,hyprworkspace 7
+      bind=$mainMod,8,exec,hyprworkspace 8
+      bind=$mainMod,9,exec,hyprworkspace 9
+
+      bind=$mainMod SHIFT,1,movetoworkspace,1
+      bind=$mainMod SHIFT,2,movetoworkspace,2
+      bind=$mainMod SHIFT,3,movetoworkspace,3
+      bind=$mainMod SHIFT,4,movetoworkspace,4
+      bind=$mainMod SHIFT,5,movetoworkspace,5
+      bind=$mainMod SHIFT,6,movetoworkspace,6
+      bind=$mainMod SHIFT,7,movetoworkspace,7
+      bind=$mainMod SHIFT,8,movetoworkspace,8
+      bind=$mainMod SHIFT,9,movetoworkspace,9
+
       monitor=Virtual-1,1680x1050,auto,1
       monitor=,1680x1050,auto,1
     '';

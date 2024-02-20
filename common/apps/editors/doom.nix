d4376cded06b8d75500ca20068cab9810101937b
Shawn Borton <shawn@shilohsvillage.com>
2024-02-19 19:36:50 -0600

    Install Emacs

---
+++ common/apps/editors/doom.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+{
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+    }))
+    (self: super: {
+      doomEmacsBase = super.emacsGit.override {
+        withGTK3 = true;
+        withNativeCompilation = true;
+        withTreeSitter = true;
+        withXwidgets = true;
+      };
+    })
+  ];
+
+  home.packages = with pkgs; [
+    doomEmacsBase
+  ]
+}

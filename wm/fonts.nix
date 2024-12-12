0ca1152a3e155f0ed583b1a1f03a1233e5f34e75
Shawn Borton <shawn@shawnborton.info>
2024-12-11 18:27:23 -0600

    Font Updates prior to System Update
    The font-related packages moved with the most recent update on 12/10/2024.

--- wm/fonts.nix
+++ wm/fonts.nix
@@ -3,7 +3,11 @@
 {
   #environment.systemPackages = with pkgs; [
   fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "Inconsolata" "Noto" "JetBrainsMono" "SourceCodePro" ]; })
+    # (nerdfonts.override { fonts = [ "Inconsolata" "Noto" "JetBrainsMono" "SourceCodePro" ]; })
+    nerd-fonts.inconsolata
+    nerd-fonts.noto
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.sauce-code-pro
     font-awesome
     inconsolata
     inconsolata-nerdfont
